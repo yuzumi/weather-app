5e74a61853acf7745ba587004816227a74419a44
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-11-17 15:20:40 +0200

    feat(store): create store

---
+++ src/store/index.ts
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import weatherReducer from 'store/reducers/weatherReducer';
+import alertReducer from 'store/reducers/alertReducer';
+
+const rootReducer = combineReducers({
+  weather: weatherReducer,
+  alert: alertReducer
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(
+      thunk
+    )
+  )
+);
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default store;
