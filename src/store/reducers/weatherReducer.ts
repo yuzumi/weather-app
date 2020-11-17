b0aa2d9071184861eff73340d37b6d1ac5987eec
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2020-11-17 15:00:11 +0200

    feat(store): create weather reducer

---
+++ src/store/reducers/weatherReducer.ts
@@ -0,0 +1,36 @@
+import { WeatherState, WeatherData, WeatherAction, GET_WEATHER, SET_LOADING, SET_ERROR } from 'store/types';
+
+const initialState: WeatherState = {
+  data: {} as WeatherData,
+  loading: false,
+  error: ''
+};
+
+const weatherReducer = (
+  state = initialState, 
+  action: WeatherAction
+): WeatherState => {
+  switch (action.type) {
+    case GET_WEATHER: 
+      return {
+        data: action.payload,
+        loading: false,
+        error: ""
+      };
+    case SET_LOADING: 
+      return {
+        ...state,
+        loading: true
+      };
+    case SET_ERROR: 
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default weatherReducer;
